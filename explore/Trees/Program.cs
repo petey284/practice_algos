0fe863d413eb0c30fd2b03c8b76a61314bbe288b
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-02-15 23:09:04 -0500

    Implemented generic tree classes..
    With String and Int as initial tree node types

--- explore/Trees/Program.cs
+++ explore/Trees/Program.cs
@@ -2,11 +2,98 @@
 
 namespace Trees
 {
+    using StringTree = Tree<string>;
+    using IntTree = Tree<int>;
+
+    public enum Traversal
+    {
+        Inorder,
+        Postorder,
+        Preorder
+    }
+
+    public class Tree<T>
+        where T: IComparable
+    {
+        public TreeNode<T> Root{ get; set; }
+
+        public void Insert(T value)
+        {
+            if (Root == null)
+            {
+                Root = new TreeNode<T>(value);
+            } else
+            {
+                Root.Insert(value);
+            }
+        }
+
+        public void Print(Traversal traverse)
+        {
+            if (traverse == Traversal.Inorder)
+            {
+
+            } else if (traverse == Traversal.Postorder)
+            {
+
+            } else
+            {
+
+            }
+        }
+    }
+
+    public class TreeNode<T>
+        where T: IComparable
+    {
+        private T data { get; set; }
+        private TreeNode<T> Left { get; set; }
+        private TreeNode<T> Right { get; set; }
+
+        public TreeNode(T data)
+        {
+            this.data = data;
+        }
+
+        public void Insert(T value)
+        {
+            // If duplicate value, then noop
+            if (value.CompareTo(this.data) == 0)
+            {
+                return;
+            }
+
+            if (value.CompareTo(this.data) < 0)
+            {
+                if (Left == null)
+                {
+                    Left = new TreeNode<T>(value);
+                } else {
+                    Left.Insert(value);
+                }
+            } else  {
+
+                if (Right == null)
+                {
+                    Right = new TreeNode<T>(value);
+                } else
+                {
+                    Right.Insert(value);
+                }
+            }
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             // Implement basic tree ADT
+
+            // Tree with string type
+            var stringTree = new StringTree();
+            stringTree.Insert("Hello");
         }
     }
 }
